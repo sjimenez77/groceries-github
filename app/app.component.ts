8c972a290c080dd17a5e6bfe7a9b97c9e48a8b49
Santos Jiménez <sjimenez77@gmail.com>
2017-02-05 12:32:08 +0100

    Chapter 3 finished http://docs.nativescript.org/angular/tutorial/ng-chapter-3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
-      <Button text="Sign up for Groceries"></Button>
-    </StackLayout>
-  `,
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  selector: "main",
+  template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-  submit() {
-    console.log(`hello ${new Date()}`);
-  }
-}
+export class AppComponent {}
